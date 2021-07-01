ecef6855d2093ff87b6898b6d7ab96b667c56c23
Karenrodriguezlab <84875960+Karenrodriguezlab@users.noreply.github.com>
2021-06-30 20:27:39 -0400

    Update setup-prod.sh

--- build/setup-prod.sh
+++ build/setup-prod.sh
@@ -10,4 +10,4 @@ echo "Conectar Sodexo"
 sfdx alias:set my-hub-orgUAT2=consultor_force@nectia.com.peru.prod.uat2
 sfdx auth:jwt:grant --clientid $SFDC_UAT_CLIENTID_CLIENTID --jwtkeyfile JWT/server.key --username $SFDC_UAT_USER --instanceurl https://test.salesforce.com
 sfdx alias:set my-hub-Prod=consultor_force@nectia.com.peru.prod
-sfdx auth:jwt:grant --clientid $SFDC_PROD_CLIENTID --jwtkeyfile JWT/server.key --username $SFDC_PROD_USER --instanceurl https://login.salesforce.com
+sfdx auth:jwt:grant --clientid $SFDC_PROD_CLIENTID --jwtkeyfile JWT/server.key --username $SFDC_PROD_USER --instanceurl https://sodexo-brs-peru.my.salesforce.com/
